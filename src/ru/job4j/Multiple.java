7df00c75171872a4db6518923c6f3ab8f67efb4b
mason <mason2802@gmail.com>
2020-11-17 21:59:02 +0300

    3.3. Разбор первой программы. [#411519]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%n%d * %d = %d",1,i,i);
+        }
+    }
+}
